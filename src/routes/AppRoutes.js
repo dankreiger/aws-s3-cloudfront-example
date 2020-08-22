0aed9c0cdd0582b5ecc7c8000b7857e33e418766
Dan Kreiger <dan@dankreiger.com>
2020-08-22 23:21:21 +0200

    chore(routes): add not found

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import About from 'pages/About';
 import Home from 'pages/Home';
 import Contact from 'pages/Contact';
-
+import NotFound from 'pages/NotFound';
 export default function AppRoutes() {
   return (
     <Switch>
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/" component={Home} />
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 }
