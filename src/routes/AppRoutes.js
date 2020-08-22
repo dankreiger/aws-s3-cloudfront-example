e8aeef97d8e15af86f76add6b7dadc9c56acd01f
Dan Kreiger <dan@dankreiger.com>
2020-08-22 23:13:23 +0200

    chore(routes): add contact route

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import About from 'pages/About';
 import Home from 'pages/Home';
+import Contact from 'pages/Contact';
 
 export default function AppRoutes() {
   return (
     <Switch>
       <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
       <Route path="/" component={Home} />
     </Switch>
   );
